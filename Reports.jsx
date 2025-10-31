283dcf34efc63d64e1f7e31564af9305f8d409f8
henrikheg <47nfz25b26@privaterelay.appleid.com>
2025-10-31 22:13:52 +0100

    Delete Reports.jsx

--- Reports.jsx
+++
@@ -1,8 +0,0 @@
-import React from'react'
-import{exportDB,importDB}from'../src/api'
-export default function Reports(){
-  const onImport=async e=>{const f=e.target.files[0];if(!f)return;await importDB(f);alert('Importert.');e.target.value=''}
-  return(<div className='container'><div className='card'><h3>Rapporter & Backup</h3>
-  <div className='row'><button className='btn' onClick={exportDB}>Eksporter database</button>
-  <label className='btn'><input type='file' accept='.db' onChange={onImport} style={{display:'none'}}/>Importer database</label></div>
-  </div></div>)}
